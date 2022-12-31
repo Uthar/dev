0d401b6d6c4147eac0db0489f39b2b644ffffe8e
Kasper Gałkowski <k@galkowski.xyz>
2022-12-31 13:07:48 +0100

    SBCL -> 2.3.0

--- sbcl.nix
+++ sbcl.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
 
   pname = "sbcl";
 
-  version = "2.2.11";
+  version = "2.3.0";
 
   buildInputs = let
     zstd' = zstd.override { static = true; };
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/project/sbcl/sbcl/${version}/sbcl-${version}-source.tar.bz2";
-    hash = "sha256-NgfWgBZzGICEXO1dXVXGBUzEnxkSGhUCfmxWB66Elt8";
+    hash = "sha256-v3Q5SXEq4Cy3ST87i1fOJBlIv2ETHjaGDdszTaFDnJc=";
   };
 
   postPatch = ''
