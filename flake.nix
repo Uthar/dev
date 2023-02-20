423d492d76702c1348fb42ff2db7cd5411818ad4
Kasper Gałkowski <kgalkowski@comscore.com>
2023-02-20 12:19:27 +0100

    Mały docker z Javą

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+
 {
 
   description = "Development environment";
@@ -15,6 +16,21 @@
     in {
       packages = rec {
         jdk = pkgs.jdk17;
+        jdk_minimal = pkgs.jdk17.override {
+          headless = true;
+          enableJavaFX = false;
+          enableGnome2 = false;
+        };
+        jre_minimal = pkgs.jre_minimal.override {
+          jdk = jdk_minimal;
+        };
+        jre_docker = pkgs.dockerTools.buildImage {
+          name = "jre";
+          tag = "latest";
+          contents = [
+            jre_minimal
+          ];
+        };
         clojure = pkgs.callPackage ./clojure.nix { inherit jdk ant; };
         cider = pkgs.callPackage ./cider.nix { inherit cljpkgs; };
         sbcl = pkgs.callPackage ./sbcl.nix {};
