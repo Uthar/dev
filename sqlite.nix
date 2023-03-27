015d105b40bdeab194252351cee8359ffa474fbc
Kasper Gałkowski <k@galkowski.xyz>
2023-03-27 17:55:24 +0200

    sqlite: buduj libsqlite3.a

--- sqlite.nix
+++ sqlite.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
 
   configureFlags = [ "--enable-threadsafe" "--enable-readline" ];
 
+  dontDisableStatic = true;
+
   NIX_CFLAGS_COMPILE = toString ([
     "-DSQLITE_ENABLE_COLUMN_METADATA"
     "-DSQLITE_ENABLE_DBSTAT_VTAB"
