2d2da80c3ab2c234a0bb74b955211cb0ba97dd69
Kasper Gałkowski <kgalkowski@comscore.com>
2024-09-09 15:30:06 +0200

    Podbiłem wydanie SQLite żeby móc użyć json_pretty

--- sqlite.nix
+++ sqlite.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "sqlite";
-  version = "3.46.0";
+  version = "3.46.1";
 
   # https://sqlite.org/news.html
   src = fetchurl {
-    url = "https://sqlite.org/2024/sqlite-autoconf-3460000.tar.gz";
-    hash = "sha256-b45qezNSc3SIFvmztiu9w3Koid6HgtfwSMZTpEdBen0=";
+    url = "https://sqlite.org/2024/sqlite-autoconf-3460100.tar.gz";
+    hash = "sha256-Z9P+bSaObq3crjcn/OWPzI6cU4ab3Qegxh443fKWUHE=";
   };
 
   buildInputs = [ zlib readline ];
