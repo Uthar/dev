9bc7ac2f7d8a8ac2e3d885cb180459a08a311ec4
Kasper Gałkowski <galkowskikasper@gmail.com>
2024-10-26 18:45:49 +0200

    wyczyściłem build gita

--- git.nix
+++ git.nix
@@ -9,52 +9,58 @@
 , expat
 , openssl
 , perl
+, python3
 , tcl
 , tk
 , asciidoc
-, libxslt
-, docbook2x
+, xmlto
+, docbook_xsl
 , docbook_xml_dtd_45
-, texinfo
+, withPython ? false
 , withPerl ? false
 , withTcltk ? false
-, withDocs ? true
+, withMan ? true
 }:
 
-let
-  opts = lib.concatMapStringsSep " " (x: "${x}=y") (
-    [ "INSTALL_SYMLINKS" "NO_PYTHON" ]
-    ++ (lib.optional (!withPerl) "NO_PERL")
-    ++ (lib.optional (!withTcltk) "NO_TCLTK")
-  );
-in stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: {
   name = "git";
   version = "2.45.2";
+  
   src = fetchFromGitHub {
     owner = "uthar";
     repo = "git";
     rev = "7df17f3b76b74066f5eb8296811efaf7090d7e32";
     hash = "sha256-gGw2Z+rc8n9ftx5yjnIluwxEMW7yzDtFckcXBPK3v7w=";
   };
-  nativeBuildInputs = [ installShellFiles ];
+  
+  nativeBuildInputs = [ installShellFiles ]
+    ++ (lib.optionals withMan [ perl ])
+    ++ (lib.optionals withMan [ asciidoc xmlto docbook_xsl docbook_xml_dtd_45 ]);
+  
   buildInputs = [ zlib sqlite curl gettext expat openssl ]
-    ++ (lib.optionals (withPerl || withDocs) [ perl ])
-    ++ (lib.optionals withTcltk [ tcl tk ])
-    ++ (lib.optionals withDocs [ asciidoc libxslt texinfo docbook2x docbook_xml_dtd_45 ]);
-  postPatch = lib.optionalString (withPerl || withDocs) ''
-    substituteInPlace Makefile --replace /usr/bin/perl ${perl}/bin/perl
+    ++ (lib.optionals withPerl [ perl ])
+    ++ (lib.optionals withPython [ python3 ])
+    ++ (lib.optionals withTcltk [ tcl tk ]);
+  
+  makeFlags = [ "INSTALL_SYMLINKS=y" ]
+    ++ (lib.optional (withPython) "PYTHON_PATH=${python3}/bin/python")
+    ++ (lib.optional (withPerl || withMan) "PERL_PATH=${perl}/bin/perl")
+    ++ (lib.optional (!withPython) "NO_PYTHON=y")
+    ++ (lib.optional (!withPerl) "NO_PERL=y")
+    ++ (lib.optional (!withTcltk) "NO_TCLTK=y");
+  
+  preBuild = ''
+    makeFlagsArray+=("prefix=$out")
   '';
-  DEFAULT_HELP_FORMAT = "info";
-  buildPhase = ''
-    make prefix=$out ${opts} all
-  '' + lib.optionalString withDocs ''
-    make prefix=$out ${opts} DOCBOOK2X_TEXI=docbook2texi info
-  '';
-  installPhase = ''
-    make prefix=$out ${opts} install
+  
+  buildFlags = [ "all" ]
+    ++ (lib.optional withMan "man");
+
+  installTargets = [ "install" ]
+    ++ (lib.optional withMan "install-man");
+
+  postInstall = ''
     installShellCompletion --bash contrib/completion/git-completion.bash
     installShellCompletion --zsh contrib/completion/git-completion.zsh
-  '' + lib.optionalString withDocs ''
-    make prefix=$out ${opts} DOCBOOK2X_TEXI=docbook2texi install-info
   '';
 })
